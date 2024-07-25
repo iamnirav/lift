32221c2505445f1636cbf4b4197866b59c6ea9e3
Nirav Sanghani <nirav.sanghani@gmail.com>
2024-07-25 09:07:07 -0700

    static site render

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto'
+// import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,11 +8,21 @@ const config = {
   // for more information about preprocessors
   preprocess: vitePreprocess(),
 
+  // kit: {
+  //   // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+  //   // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+  //   // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+  //   adapter: adapter(),
+  // },
+
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+    }),
   },
 
   compilerOptions: {
