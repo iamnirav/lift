7d9aa1f9bf9b2db445bb159160628984b9803033
Nirav Sanghani <nirav.sanghani@gmail.com>
2024-07-28 12:21:32 -0700

    add Inter font, use set boxes for rest signal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,14 @@
 
   let now = $state(0) // updated every second to be Date.now()
   let startTime = $state(0) // timestamp the workout was started
-  let segmentStartTime = $state(0) // timestamp this segment was started
+  let restStartTime = $state(0) // timestamp this rest was started
   let segmentsComplete = $state(0) // how many sets (including warmup) have been completed
   let running = $state(false) // if the workout has been started
   let weight = $state(0) // total weight
 
   function start() {
     startTime = Date.now()
-    segmentStartTime = Date.now()
+    restStartTime = Date.now()
     running = true
   }
 
@@ -23,7 +23,7 @@
   }
 
   function completeSegment() {
-    segmentStartTime = Date.now()
+    restStartTime = Date.now()
     segmentsComplete++
   }
 
@@ -51,40 +51,66 @@
     weight,
     weight,
   ])
+
+  const timeElapsed = $derived(now - startTime)
+  const restTimeElapsed = $derived(now - restStartTime)
 </script>
 
-<h1
-  class:wait={running && now - segmentStartTime < REST_DURATION}
-  class:go={running &&
-    (now - segmentStartTime >= REST_DURATION || segmentsComplete === 0)}
->
-  Lift {#if weight}{weight} lbs{/if}
+<h1>
+  Lift{#if weight}{weight} lbs{/if}
 </h1>
 
 <p>
   {#each segments as segment, index}
     <span
       class:complete={running && index < segmentsComplete}
-      class:current={running && index === segmentsComplete}>{segment}</span
+      class:rest={running &&
+        index === segmentsComplete &&
+        restTimeElapsed < REST_DURATION}
+      class:go={running &&
+        index === segmentsComplete &&
+        (restTimeElapsed >= REST_DURATION || segmentsComplete === 0)}
+      >{segment}</span
     >
   {/each}
 </p>
 
-<p>Rest time: {running ? formatDate(now - segmentStartTime) : '00:00'}</p>
-<p>Total time: {running ? formatDate(now - startTime) : '00:00'}</p>
-
-<button onclick={getWeight}>Set Weight</button>
-
-<button onclick={start} disabled={running || !weight}>Start</button>
-<button onclick={reset} disabled={!running}>Reset</button>
 <button onclick={completeSegment} disabled={!running}>Complete Set</button>
 
+<p>Rest time: {running ? formatDate(restTimeElapsed) : '00:00'}</p>
+<p>Total time: {running ? formatDate(timeElapsed) : '00:00'}</p>
+
+<button onclick={getWeight}>Set Weight</button>
+<button onclick={start} disabled={running || !weight}>Start</button>
+<button onclick={reset} disabled={!running}>Reset</button>
+
 <style>
+  :root {
+    font-family: Inter, sans-serif;
+    font-feature-settings:
+      'liga' 1,
+      'calt' 1; /* fix for Chrome */
+  }
+  @supports (font-variation-settings: normal) {
+    :root {
+      font-family: InterVariable, sans-serif;
+    }
+  }
+
   span {
-    margin-right: 10px;
     display: inline-block;
     border: 1px solid black;
     padding: 5px 10px;
+    border-right-width: 0;
+
+    &:first-child {
+      border-radius: 2px 0 0 2px;
+    }
+
+    &:last-child {
+      border-right-width: 1px;
+      border-radius: 0 2px 2px 0;
+    }
   }
 
   span.complete {
@@ -92,16 +118,11 @@
     color: white;
   }
 
-  span.current {
-    background-color: darkgreen;
-    color: white;
+  span.rest {
+    background-color: yellow;
   }
 
-  h1.wait {
-    color: darkblue;
-  }
-
-  h1.go {
+  span.go {
     background-color: darkgreen;
     color: white;
   }
